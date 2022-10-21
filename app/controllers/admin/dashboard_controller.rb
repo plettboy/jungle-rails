a848b748b42fea5075d1f7fe2cb9422da55884a6
plettboy <ryan.plett@yahoo.com>
2022-10-21 14:54:41 +0000

    added product count to admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: "Jungle", password: "book" 
   def show
+    @admin_product_count = Product.count
+    @admin_category_count = Category.count
   end
 end
